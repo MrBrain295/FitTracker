0c23b5a0bc695c4e7aca28bba032eea6b9750692
Ryan <ryan@Ryans-iMac.local>
2021-03-02 14:35:28 -0800

    updated route file

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,63 @@
 const router = require("express").Router();
+const workout = require("../models/workout.js");
 
 
-router
+
+
+//file path? Get Workout
+router.get("/api/workouts", (req, res) => {
+
+    workout.find().then((getWorkoutData) => {
+    res.json(getWorkoutData);
+     })
+
+    .catch((err) => {
+     res.status(500).json(err);
+     });
+
+});
+
+
+//Create
+router.post("/api/workouts", (req, res) => {
+
+    workout.create({})
+    .then(workoutData =>  {
+     res.json(workoutData);
+     })
+
+      .catch(err => {
+       res.json(err);
+       });
+  });
+
+
+  //Add
+  router.put("/api/workouts/:id", ({ body, parameters }, res) => {
+  workout.getAndUpdateId(
+       parameters.id,
+      {$push: {exercise:body}},
+      {new: true}
+  ) 
+  
+  .then(workoutData =>{
+      res.json(workoutData)
+     })
+  .catch(err => {
+      res.json(err)
+    })
+
+})
+
+router.delete("/api/workouts", ({body}, res) => {
+    workout.getAndDeleteID(body.id)
+    .then(() => {
+      res.json(true);
+    })
+
+    .catch(err => {
+      res.json(err);
+    })
+})
+ module.exports = router
+
