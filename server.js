5d018081de14112bf0c0d54458555999ad6af03d
Ryan <ryan@Ryans-iMac.local>
2021-03-05 12:10:23 -0800

    updated mongodb connection to Atlas on server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+// require('dotenv').config()
 
 const PORT = process.env.PORT || 3001; 
 
@@ -15,11 +16,16 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+   mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/FitTracker',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   );
+
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/views.js"));
